8a34f66688421db12791055ffdf7eccc6328022f
Alexey <alexh2@yandex.ru>
2022-10-13 17:35:49 +0300

    hw6

---
+++ task_4.py
@@ -0,0 +1,49 @@
+# Напишите программу, которая принимает на вход число N
+# и выдает набор произведений чисел от 1 до N.
+# Пример:
+# пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+# def InputNumbers(inputText):
+#     is_OK = False
+#     while not is_OK:
+#         try:
+#             number = int(input(f"{inputText}"))
+#             is_OK = True
+#         except ValueError:
+#             print("Число должно быть integer ")
+#     return number
+
+# def mult(num):
+#     if num == 1:
+#         return 1
+#     else:
+#         return num * mult(num - 1)
+
+# num = InputNumbers("Введите число: ")
+
+# list = []
+# for e in range(1, num + 1):
+#     list.append(mult(e))
+
+# print(f"Произведения чисел от 1 до {num}:  {list}")
+
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Число должно быть integer ")
+    return number
+num = InputNumbers("Введите число: ") 
+rand_list=[i for i in range(1,num+1)]
+print(rand_list)
+def mult(num):
+    if num == 1:
+        return 1
+    else:
+        return num * mult(num - 1)
+       
+new_list = list(map(mult, rand_list))		
+print(new_list)
